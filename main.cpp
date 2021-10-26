3baddeb49fa22ac72058fc430e01abe4df4ac69b
Azydov <sanal88@yandex.ru>
2021-10-26 15:05:46 +0300

    Writing a Shader in OpenGL

--- main.cpp
+++ main.cpp
@@ -2,6 +2,50 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <memory>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source){
+  unsigned int id = glCreateShader(type);
+  const char * src = source.c_str();
+  glShaderSource(id, 1, &src, nullptr);
+  glCompileShader(id);
+
+  int result = 0;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  if(result == GL_FALSE){
+    int length = 0;
+    std::unique_ptr<char> message{new char[length]};
+
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    glGetShaderInfoLog(id, length, &length, message.get());
+
+    std::cout << "Failed to compile "<<
+                  ((type == GL_VERTEX_SHADER)? "vertex" : "fragment") <<
+                  " shader"<< std::endl;
+    std::cout << message.get() << std::endl;
+    
+    glDeleteShader(id);
+    return 0;
+  }
+
+  return id;
+}
+
+static int CreateProgram(const std::string& vertexShader, const std::string& fragmentShader){
+  unsigned int program = glCreateProgram();
+  unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return program;
+}
 
 int main(void)
 {
@@ -10,6 +54,14 @@ int main(void)
   /* Initialize the library */
   if (!glfwInit())
     return -1;
+
+  //Set minimal version required OpenGL.
+  //major
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  //minor
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  //Set profile for context
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -37,12 +89,33 @@ int main(void)
   };
 
   unsigned int buffer = 0;
+  glGenVertexArrays(1, &buffer);
+  glBindVertexArray(buffer);
   glGenBuffers(1, &buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
   glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+
+  std::string vertexShader = 
+      "#version 330 core\n"
+      "\n"
+      "layout(location = 0) in vec4 position;\n"
+      "void main()\n"
+      "{\n"
+      "  gl_Position = position;\n"
+      "}\n";
+  std::string fragmentShader = 
+      "#version 330 core\n"
+      "\n"
+      "layout(location = 0) out vec4 color;\n"
+      "void main()\n"
+      "{\n"
+      "  color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+      "}\n";
+  unsigned int program = CreateProgram(vertexShader, fragmentShader);
+  glUseProgram(program);
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
@@ -58,6 +131,8 @@ int main(void)
     /* Poll for and process events */
     glfwPollEvents();
   }
+
+  glDeleteProgram(program);
 
   glfwTerminate();
   return 0;
