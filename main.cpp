f8ee2abd4966303718a3f3bfe1ae3e3525c646c5
Azydov <sanal88@yandex.ru>
2021-11-11 20:05:30 +0300

    ImGui in OpenGL

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 #include <iostream>
 #include <memory>
 #include <fstream>
@@ -21,8 +25,6 @@
 
 int main(void)
 {
-  GLFWwindow *window;
-
   /* Initialize the library */
   if (!glfwInit())
     return -1;
@@ -36,7 +38,7 @@ int main(void)
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  GLFWwindow * window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
   if (!window)
   {
     glfwTerminate();
@@ -100,13 +102,28 @@ int main(void)
 
   Renderer renderer;
 
+  ImGui::CreateContext();
+  ImGui::StyleColorsDark();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  const char* glsl_version = "#version 330";
+  ImGui_ImplOpenGL3_Init(glsl_version);
+
   float r = 0.0f;
   float increment = 0.05f;
+
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
   {
     /* Render here */
     renderer.Clear();
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
     shader.Bind();
     //shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
@@ -120,6 +137,30 @@ int main(void)
     }
     r += increment;
 
+    {
+      static float f = 0.0f;
+      static int counter = 0;
+
+      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+
+      ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
+      ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+      ImGui::Checkbox("Another Window", &show_another_window);
+
+      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+      ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+
+      if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+        counter++;
+      ImGui::SameLine();
+      ImGui::Text("counter = %d", counter);
+
+      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      ImGui::End();
+    }
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     /* Swap front and back buffers */
     GLCall(glfwSwapBuffers(window));
 
@@ -127,6 +168,10 @@ int main(void)
     GLCall(glfwPollEvents());
   }
 
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+  glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
 }
